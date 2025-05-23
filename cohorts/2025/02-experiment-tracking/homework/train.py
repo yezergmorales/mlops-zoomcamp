429b137bf038504481ae219660ac1e3ebb265ffd
yezergmorales <yezergm@gmail.com>
2025-05-23 17:24:49 +0100

    homework 2

--- cohorts/2025/02-experiment-tracking/homework/train.py
+++ cohorts/2025/02-experiment-tracking/homework/train.py
@@ -1,10 +1,12 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
+mlflow.sklearn.autolog(disable=False)
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -12,22 +14,18 @@ def load_pickle(filename: str):
 
 
 @click.command()
-@click.option(
-    "--data_path",
-    default="./output",
-    help="Location where the processed NYC taxi trip data was saved"
-)
+@click.option("--data_path", default="./output", help="Location where the processed NYC taxi trip data was saved")
 def run_train(data_path: str):
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_train()
